ce9cb2aabf11fd1f60f9c975e1b50af5fcf14c11
Natthaphon Pachanawan <pachanawan.p@gmail.com>
2025-04-16 00:11:45 +0700

    สร้าง cors ในการเชื่อมกับหน้าบ้าน

---
+++ config/cors.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['http://localhost:4200'], // หรือ ['*'] ถ้าทดสอบเฉย ๆ
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
